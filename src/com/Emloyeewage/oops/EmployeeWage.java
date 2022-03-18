87f06a9791cc2dfec5c1aa8e12887ee4e1ec3632
bhadanebhushan <bhadanebhushan97@gmail.com>
2022-03-18 14:51:34 +0530

    Calculating one day salary of employee

--- src/com/Emloyeewage/oops/EmployeeWage.java
+++ src/com/Emloyeewage/oops/EmployeeWage.java
@@ -3,19 +3,32 @@ package com.Emloyeewage.oops;
 import java.util.Random;
 
 public class EmployeeWage {
-	
-public static void main(String[] args) {
-		
+	public static void main(String[] args) {
 		System.out.println("Welcome to empwage");
-			Random ran = new Random();
-			int present = 1;
-			int empCheck = ran.nextInt(2);
-			if (empCheck == present) {
-				System.out.println("Employee is Present");
-			}
-			else {
-				System.out.println("Employee is Absent");
-			}
-		
+
+		int empCheck = Employee.empCheck();
+		Employee.empSalary(empCheck);
+	}
+
+}
+class Employee {
+	static final int is_full_time = 1;
+	static final int Emp_Rate_Per_Hr= 20;
+
+	public static int empCheck() {
+		Random random = new Random();
+		int empCheck = random.nextInt(2);
+		return empCheck;
+	}
+
+	public static void empSalary(int empCheck) {
+		int empWrkHrs = 0;
+		if (empCheck == is_full_time) {
+			empWrkHrs = 8;
+		} else {
+			empWrkHrs = 0;
 		}
+		int empWage = empWrkHrs * Emp_Rate_Per_Hr;
+		System.out.println("empWage = " + empWage);
+	}
 }

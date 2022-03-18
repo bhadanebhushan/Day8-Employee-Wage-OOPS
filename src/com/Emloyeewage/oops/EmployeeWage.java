3ab3562319cfbc129ea593a685677e3697bdc421
bhadanebhushan <bhadanebhushan97@gmail.com>
2022-03-18 14:46:47 +0530

    using OOPS check emp presnet or absent

--- src/com/Emloyeewage/oops/EmployeeWage.java
+++ src/com/Emloyeewage/oops/EmployeeWage.java
@@ -1,8 +1,21 @@
 package com.Emloyeewage.oops;
 
+import java.util.Random;
+
 public class EmployeeWage {
 	
-	public static void main(String[] args) {
+public static void main(String[] args) {
+		
 		System.out.println("Welcome to empwage");
-	}
+			Random ran = new Random();
+			int present = 1;
+			int empCheck = ran.nextInt(2);
+			if (empCheck == present) {
+				System.out.println("Employee is Present");
+			}
+			else {
+				System.out.println("Employee is Absent");
+			}
+		
+		}
 }
